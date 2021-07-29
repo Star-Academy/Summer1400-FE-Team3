9192b6b1819c0fc1bfd84af1c2ae844dcb1d860e
kimiasedighi <kimiasedighi2000@gmail.com>
2021-07-29 15:50:06 +0430

    format code

--- scripts/songs.js
+++ scripts/songs.js
@@ -1,7 +1,11 @@
-let songArr = [{name:"Always", genre:"metal", singer:"isak"}, {name:"Hello", genre:"pop", singer:"adel"}, 
-{name:"blinding lights", genre:"rock", singer:"the weekend"}, {name:"ocean eyes", genre:"rap", singer:"billie"},
-{name:"Diamonds", genre:"pop", singer:"sam smith"}, {name:"bad", genre:"country", singer:"james"}
-]
+let songArr = [
+  { name: "Always", genre: "metal", singer: "isak" },
+  { name: "Hello", genre: "pop", singer: "adel" },
+  { name: "blinding lights", genre: "rock", singer: "the weekend" },
+  { name: "ocean eyes", genre: "rap", singer: "billie" },
+  { name: "Diamonds", genre: "pop", singer: "sam smith" },
+  { name: "bad", genre: "country", singer: "james" },
+];
 
 let songList = document.getElementById("songList");
 let artistSelect = document.getElementById("artist");
@@ -10,34 +14,32 @@ let filterButton = document.getElementById("filterButton");
 let searchButton = document.getElementById("searchButton");
 let searchInput = document.getElementById("search");
 
-function makeList() {   
-    songArr.forEach((x)=> {songList.innerHTML += "<li><a href=\"Song.html\">" + x.name+ "</a></li>"; });      
+function makeList() {
+  songArr.forEach((x) => {
+    songList.innerHTML += '<li><a href="Song.html">' + x.name + "</a></li>";
+  });
 }
-    
+
 makeList();
 
 searchButton.addEventListener("click", () => {
-    var searchValue = searchInput.value.toLowerCase();
-    songList.innerHTML = "";
-    songArr.forEach(
-        (x) => {
-            if(x.name.toLowerCase().includes(searchValue)){
-                songList.innerHTML += "<li><a href=\"Song.html\">" + x.name+ "</a></li>";
-            }
-        }
-    )
-})
+  var searchValue = searchInput.value.toLowerCase();
+  songList.innerHTML = "";
+  songArr.forEach((x) => {
+    if (x.name.toLowerCase().includes(searchValue)) {
+      songList.innerHTML += '<li><a href="Song.html">' + x.name + "</a></li>";
+    }
+  });
+});
 
 filterButton.addEventListener("click", () => {
-    var artist = artistSelect.value.toLowerCase();
-    var genre = genreSelect.value.toLowerCase();
-    
-    songList.innerHTML = "";
-    songArr.forEach(
-        (x) => {
-            if(x.singer.toLowerCase()==(artist) && x.genre.toLowerCase()==genre){
-                songList.innerHTML += "<li><a href=\"Song.html\">" + x.name+ "</a></li>";
-            }
-        }
-    )
-})
+  var artist = artistSelect.value.toLowerCase();
+  var genre = genreSelect.value.toLowerCase();
+
+  songList.innerHTML = "";
+  songArr.forEach((x) => {
+    if (x.singer.toLowerCase() == artist && x.genre.toLowerCase() == genre) {
+      songList.innerHTML += '<li><a href="Song.html">' + x.name + "</a></li>";
+    }
+  });
+});
--- scripts/user.js
+++ scripts/user.js
@@ -5,11 +5,14 @@ let songArr = ["Always", "Diamonds", "Blinding Lights"];
 let favArr = ["Always", "Yellow"];
 
 function makeList() {
-    
-    songArr.forEach((x)=> {songList.innerHTML += "<li><a href=\"Song.html\">" + x+ "</a></li>"; })
-    
-    favArr.forEach((x)=> {favList.innerHTML += "<li><a href=\"Song.html\">" + x+ "</a></li>"; })
-    console.log("here")
-    }
-    
-    makeList();
+  songArr.forEach((x) => {
+    songList.innerHTML += '<li><a href="Song.html">' + x + "</a></li>";
+  });
+
+  favArr.forEach((x) => {
+    favList.innerHTML += '<li><a href="Song.html">' + x + "</a></li>";
+  });
+  console.log("here");
+}
+
+makeList();
