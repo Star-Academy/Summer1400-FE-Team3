09733bbb8182a047fe4cf38092004ad7ecb05ddd
Parmida Khani <khani1379@yahoo.com>
2021-08-03 14:22:45 +0430

    fetch user data

---
+++ scripts/signup.js
@@ -0,0 +1,47 @@
+// submit a new user data
+let username = document.getElementById("username");
+let email = document.getElementById("email");
+let password = document.getElementById("password");
+let firstName = document.getElementById("firstName");
+let lastName = document.getElementById("lastName");
+
+
+
+// let submitButton = document.getElementById("submitButton");
+// submitButton.addEventListener("click", submit);
+
+let registerForm = document.getElementById("registerForm");
+registerForm.addEventListener("submit", submit);
+
+async function submit(event){
+    event.preventDefault();
+    let user = {
+        username: username.value,
+        email: email.value,
+        password: password.value,
+        firstName: firstName.value,
+        lastName: lastName.value
+    }
+    
+    let response = await fetch('http://130.185.120.192:5000/user/register', {
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json;charset=utf-8'
+        },
+        body: JSON.stringify(user)
+      });     
+      const result = await response.json();
+      
+      if(response.status===200){
+          alert("You submitted successfully!");
+          localStorage.setItem('token', result.token);
+          window.location.href="./User.html";
+          
+      }else if(response.status===400){
+          alert("Bad Request!");
+          console.log(result);
+      }else{
+          alert("Server Error!");
+          console.log(result);
+      }
+}
