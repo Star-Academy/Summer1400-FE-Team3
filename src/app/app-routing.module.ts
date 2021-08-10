3744fe99dd56711ee330dbd3d914cf89ed4dc20f
Parmida Khani <khani1379@yahoo.com>
2021-08-10 17:24:07 +0430

    start angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,10 +1,19 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import {HomeComponent} from "./home";
+import {SignInComponent} from "./signIn";
+import {SignUpComponent} from "./signUp";
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path:'', component: HomeComponent},
+  {path:'signIn', component:SignInComponent},
+  {path:'signUp', component:SignUpComponent},
+  {path:'**', redirectTo:''}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
+export const appRoutingModule= RouterModule.forRoot(routes);
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,20 +3,29 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-import { HeaderComponent } from './header/header.component';
-import { BackgroundComponent } from './background/background.component';
-import { FooterComponent } from './footer/footer.component';
+import { HomeComponent } from './home';
+import { HeaderComponent } from './home/header/header.component';
+import { BackgroundComponent } from './home/background/background.component';
+import { FooterComponent } from './home/footer/footer.component';
+// @ts-ignore
+import { SignInComponent } from './signIn';
+import { SignUpComponent } from './signUp';
+import {appRoutingModule} from "./app-routing.module";
 
 @NgModule({
   declarations: [
     AppComponent,
     HeaderComponent,
     BackgroundComponent,
-    FooterComponent
+    FooterComponent,
+    HomeComponent,
+    SignInComponent,
+    SignUpComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    appRoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
