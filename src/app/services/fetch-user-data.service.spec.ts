c7ba65d88c3f5181cc4b90432e5e111e7ba40965
Parmida Khani <khani1379@yahoo.com>
2021-08-18 17:54:08 +0430

    trying to write test!!

--- src/app/services/fetch-user-data.service.spec.ts
+++ src/app/services/fetch-user-data.service.spec.ts
@@ -1,16 +1,37 @@
 import { TestBed } from '@angular/core/testing';
 
 import { FetchUserDataService } from './fetch-user-data.service';
+import {SignInModel_username} from "../models";
+import {of} from "rxjs";
+import {HttpClientTestingModule} from "@angular/common/http/testing";
 
 describe('FetchDataService', () => {
-  let service: FetchUserDataService;
-
+  // let service: FetchUserDataService;
+  let fetchUserDataService:FetchUserDataService, mockHttp:any;
   beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [FetchUserDataService]
+    })
+    mockHttp=jasmine.createSpyObj('mockhttp',['post']);
+    fetchUserDataService=new FetchUserDataService(mockHttp);
     TestBed.configureTestingModule({});
-    service = TestBed.inject(FetchUserDataService);
+    fetchUserDataService = TestBed.inject(FetchUserDataService);
   });
 
   it('should be created', () => {
-    expect(service).toBeTruthy();
+    expect(fetchUserDataService).toBeTruthy();
   });
+  describe('signInSubmit',()=>{
+    it('should call http.post with the right body', function () {
+      let user:SignInModel_username={
+        username:"parmida",
+        password:"12345678"
+      }
+      let userString='"username":"parmida","password":"12345678"';
+      mockHttp.post.and.returnValue(of(false));
+      fetchUserDataService.signInSubmit(user);
+      expect(mockHttp.post).toHaveBeenCalledWith('https://songs.code-star.ir/user/login',userString,jasmine.any(Object))
+    });
+  })
 });
--- src/app/signIn/sign-in-form/sign-in-form.component.spec.ts
+++ src/app/signIn/sign-in-form/sign-in-form.component.spec.ts
@@ -1,12 +1,19 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
 
 import { SignInFormComponent } from './sign-in-form.component';
+import {HttpClientTestingModule} from "@angular/common/http/testing";
+import {FetchUserDataService} from "../../services/fetch-user-data.service";
+import {RouterTestingModule} from "@angular/router/testing";
 
 describe('SignInFormComponent', () => {
   let component: SignInFormComponent;
   let fixture: ComponentFixture<SignInFormComponent>;
 
   beforeEach(async () => {
+    TestBed.configureTestingModule({
+      imports:[HttpClientTestingModule,RouterTestingModule],
+      providers: [SignInFormComponent]
+    })
     await TestBed.configureTestingModule({
       declarations: [ SignInFormComponent ]
     })
@@ -18,8 +25,13 @@ describe('SignInFormComponent', () => {
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
-  it('should create', () => {
-    expect(component).toBeTruthy();
+  let email="parmida@gmail.com"
+  it('validate email should return true', () => {
+    expect(component.validateEmail(email)).toBeTruthy();
   });
+  email="parmida"
+  it ('validate email should return false',()=> {
+    expect(component.validateEmail(email)).toBeFalse();
+  })
+
 });
