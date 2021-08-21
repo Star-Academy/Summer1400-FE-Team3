35c2d386f41488e8337dc26e4e99d41882b16d1e
kimiasedighi <kimiasedighi2000@gmail.com>
2021-08-21 14:18:15 +0430

    test and profile

--- src/app/services/fetch-user-data.service.spec.ts
+++ src/app/services/fetch-user-data.service.spec.ts
@@ -1,37 +1,130 @@
+// import { TestBed } from '@angular/core/testing';
+
+// import {
+//   HttpClientTestingModule,
+//   HttpTestingController,
+// } from '@angular/common/http/testing';
+
+// import { FetchUserDataService } from './fetch-user-data.service';
+// import { SignInModel_username } from '../models';
+// import { of } from 'rxjs';
+
+// describe('FetchDataService', () => {
+//   // let service: FetchUserDataService;
+//   let fetchUserDataService: FetchUserDataService,
+//     mockHttp: HttpTestingController;
+//   beforeEach(() => {
+//     TestBed.configureTestingModule({
+//       imports: [HttpClientTestingModule],
+//       providers: [FetchUserDataService],
+//     });
+//     //mockHttp = jasmine.createSpyObj('mockhttp', ['post']);
+//     mockHttp = TestBed.get(HttpTestingController);
+//     fetchUserDataService = new FetchUserDataService(mockHttp);
+//     TestBed.configureTestingModule({});
+//     fetchUserDataService = TestBed.inject(FetchUserDataService);
+//   });
+
+//   it('should be created', () => {
+//     expect(fetchUserDataService).toBeTruthy();
+//   });
+//   describe('signInSubmit', () => {
+//     it('should call http.post with the right body', function () {
+//       let user: SignInModel_username = {
+//         username: 'parmida',
+//         password: '12345678',
+//       };
+//       let userString = '"username":"parmida","password":"12345678"';
+//       mockHttp.post.and.returnValue(of(false));
+//       fetchUserDataService.signInSubmit(user);
+//       expect(mockHttp.post).toHaveBeenCalledWith(
+//         'https://songs.code-star.ir/user/login',
+//         userString,
+//         jasmine.any(Object)
+//       );
+//     });
+//   });
+// });
+
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { TestBed } from '@angular/core/testing';
+import { HttpClient, HttpResponse } from '@angular/common/http';
+
+// import { Employee } from './employee';
+// import { EmployeeService } from './employee.service';
 
 import { FetchUserDataService } from './fetch-user-data.service';
-import {SignInModel_username} from "../models";
-import {of} from "rxjs";
-import {HttpClientTestingModule} from "@angular/common/http/testing";
+import { SignInModel_username } from '../models';
 
-describe('FetchDataService', () => {
-  // let service: FetchUserDataService;
-  let fetchUserDataService:FetchUserDataService, mockHttp:any;
+describe('#EmployeeService.addEmploye()', () => {
+  let httpClient: HttpClient;
+  let httpTestingController: HttpTestingController;
+  let userService: FetchUserDataService;
+
   beforeEach(() => {
+    //Configures testing app module
     TestBed.configureTestingModule({
       imports: [HttpClientTestingModule],
-      providers: [FetchUserDataService]
-    })
-    mockHttp=jasmine.createSpyObj('mockhttp',['post']);
-    fetchUserDataService=new FetchUserDataService(mockHttp);
-    TestBed.configureTestingModule({});
-    fetchUserDataService = TestBed.inject(FetchUserDataService);
+      providers: [FetchUserDataService],
+    });
+
+    //Instantaites HttpClient, HttpTestingController and EmployeeService
+    httpClient = TestBed.inject(HttpClient);
+    httpTestingController = TestBed.inject(HttpTestingController);
+    userService = TestBed.inject(FetchUserDataService);
   });
 
-  it('should be created', () => {
-    expect(fetchUserDataService).toBeTruthy();
+  afterEach(() => {
+    httpTestingController.verify(); //Verifies that no requests are outstanding.
   });
-  describe('signInSubmit',()=>{
-    it('should call http.post with the right body', function () {
-      let user:SignInModel_username={
-        username:"parmida",
-        password:"12345678"
-      }
-      let userString='"username":"parmida","password":"12345678"';
-      mockHttp.post.and.returnValue(of(false));
-      fetchUserDataService.signInSubmit(user);
-      expect(mockHttp.post).toHaveBeenCalledWith('https://songs.code-star.ir/user/login',userString,jasmine.any(Object))
+
+  //Test case 1
+  it('should add an employee and return it', () => {
+    //const newUser: SignInModel_username = { username: 'Mahesh', password: "12345678" };
+    let newUser: SignInModel_username = {
+      username: 'parmida',
+      password: '12345678',
+    };
+    let userString = '{"username":"parmida","password":"12345678"}';
+    // userService
+    //   .signInSubmit(newUser)
+    //   .subscribe(
+    //     (data) => expect(data).toEqual(newUser, 'should return the user'),
+    //     fail
+    //   );
+
+    // addEmploye should have made one request to POST employee
+    const req = httpTestingController.expectNone(
+      'https://songs.code-star.ir/user/login'
+    );
+    expect(req.request.method).toEqual('POST');
+    expect(req.request.body).toEqual(newUser);
+
+    // Expect server to return the employee after POST
+    const expectedResponse = new HttpResponse({
+      status: 201,
+      statusText: 'Created',
+      body: userString,
     });
-  })
+    req.event(expectedResponse);
+  });
+
+  //Test case 2
+  // it('should turn 404 error into return of the requested employee', () => {
+  //   const newEmp: Employee = { name: 'Mahesh', age: 25 };
+
+  //   empService.addEmployee(newEmp).subscribe(
+  //     data => expect(data).toEqual(newEmp, 'should return the employee'),
+  //     fail
+  //   );
+
+  //   const req = httpTestingController.expectOne(empService.empUrl);
+
+  //   // respond with a 404 and the error message in the body
+  //   const msg = '404 error';
+  //   req.flush(msg, { status: 404, statusText: 'Not Found' });
+  // });
 });
--- src/app/services/send-request.service.ts
+++ src/app/services/send-request.service.ts
@@ -6,29 +6,33 @@ import { Injectable } from '@angular/core';
 export class SendRequestService {
   constructor() {}
 
-  public static async sendRequest(url: string,hasJson:boolean, body?: object): Promise<any> {
+  public static async sendRequest(
+    url: string,
+    hasJson: boolean,
+    body?: object
+  ): Promise<any> {
     const init: RequestInit = {
       headers: {
         'Content-Type': 'application/json',
+        'Access-Control-Allow-Origin': 'https://songs.code-star.ir/user/alter',
+        'Cache-Control':
+          'no-cache, no-store, must-revalidate, post-check=0, pre-check=0',
+        Pragma: 'no-cache',
+        Expires: '0',
       },
     };
 
     if (body) {
       init.method = 'POST';
       init.body = JSON.stringify(body);
-      if (url.includes("https://songs.code-star.ir/user/one/"))
-        init.cache="no-cache";
-      // if (url==="https://songs.code-star.ir/user/alter")
-      //   init.mode="no-cors";
     }
 
     return fetch(url, init).then((res) => {
       if (res.ok) {
-          if (hasJson)
-            return res.json();
-          return;
+        if (hasJson) return res.json();
+        return;
       }
-      console.log("hhh")
+
       throw res.json();
     });
   }
